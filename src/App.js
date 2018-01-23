00d6ae8a37eabd45c1906621c9a77cbe1f2aa37b
sarthak <sarthak@TheSinghStory.local>
2018-01-23 17:45:48 -0500

    Data sync Parent

--- src/App.js
+++ src/App.js
@@ -19,26 +19,31 @@ class App extends Component {
 }
 
 class Square extends Component {
-    // React components can have state which should be considered private to the component
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-            {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+            {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
     }
 
     render() {
